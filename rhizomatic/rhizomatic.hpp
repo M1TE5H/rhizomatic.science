00f6c2ef93ddb98f913285608829d4d7bd4f0a0b
M1TE5H <9112440+M1TE5H@users.noreply.github.com>
2022-11-11 22:29:48 -0500

    Create rhizomatic.hpp

---
+++ rhizomatic/rhizomatic.hpp
@@ -0,0 +1,41 @@
+/**
+ *
+ * @file  rhizomatic.hpp
+ * @brief standalone include for www.rhizomatic.science
+ *
+ * RHIZOMATIC
+ *
+ * ...
+ *
+ */
+
+#ifndef __RHIZOMATIC_HPP__
+#define __RHIZOMATIC_HPP__
+
+#include <cmath>
+#include <cstddef>
+
+#include <algorithm>
+#include <iterator>
+
+// ----------------------------------------------------------------
+
+/**
+ *
+ * INTERNAL
+ *
+ * Details for the Standalone Include
+ *
+ */
+
+namespace rhizomatic {
+
+namespace internal {
+
+} // namespace internal
+  
+} // namespace rhizomatic
+
+// ----------------------------------------------------------------
+
+#endif // __RHIZOMATIC_HPP__
